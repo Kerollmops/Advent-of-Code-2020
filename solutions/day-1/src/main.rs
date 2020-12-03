6c14503d8c5cae9721984a180addee53c3487a3c
Clément Renault <clement@meilisearch.com>
2020-12-03 21:16:22 +0100

    Input and solution for day-1 part two

--- solutions/day-1/src/main.rs
+++ solutions/day-1/src/main.rs
@@ -6,7 +6,7 @@ fn main() -> anyhow::Result<()> {
     let mut expenses = INPUT.lines().map(u32::from_str).collect::<Result<Vec<_>, _>>()?;
     expenses.sort_unstable();
 
-    let answer = (0..expenses.len()).find_map(|i| {
+    let part_one_answer = (0..expenses.len()).find_map(|i| {
         expenses[i..].split_first().and_then(|(left, tail)| {
             tail.iter().find_map(|&right| {
                 if left + right == 2020 { Some(left * right) } else { None }
@@ -14,9 +14,28 @@ fn main() -> anyhow::Result<()> {
         })
     });
 
-    match answer {
-        Some(answer) => println!("answer is {}", answer),
-        None => println!("no answer found"),
+    match part_one_answer {
+        Some(answer) => println!("part one answer is {}", answer),
+        None => println!("no part one answer found"),
+    }
+
+    let part_two_answer = (0..expenses.len()).find_map(|i| {
+        expenses[i..].split_first().and_then(|(first, tail)| {
+            (0..tail.len()).find_map(|i| {
+                tail[i..].split_first().and_then(|(second, tail)| {
+                    tail.iter().find_map(|&third| if first + second + third == 2020 {
+                        Some(first * second * third)
+                    } else {
+                        None
+                    })
+                })
+            })
+        })
+    });
+
+    match part_two_answer {
+        Some(answer) => println!("part two answer is {}", answer),
+        None => println!("no part two answer found"),
     }
 
     Ok(())
