595a8b606131db588449dae996b3291733cc3723
Clément Renault <clement@meilisearch.com>
2020-12-05 12:15:12 +0100

    Remove the Goto enum

--- solutions/day-5/src/main.rs
+++ solutions/day-5/src/main.rs
@@ -1,3 +1,5 @@
+use std::str::Chars;
+
 const INPUT: &str = include_str!("../../../inputs/day-5.txt");
 
 fn main() -> anyhow::Result<()> {
@@ -9,43 +11,31 @@ fn main() -> anyhow::Result<()> {
     seat_ids.sort_unstable();
 
     let part_one_answer = seat_ids.last().unwrap();
-    println!("part one anwser is {}", part_one_answer);
+    println!("part one answer is {}", part_one_answer);
 
     let part_two_answer = seat_ids.windows(2).find(|ws| ws[0] + 1 != ws[1]).unwrap()[0] + 1;
-    println!("part two anwser is {}", part_two_answer);
+    println!("part two answer is {}", part_two_answer);
 
     Ok(())
 }
 
 fn seat_row_column(input: &str) -> (u32, u32) {
     let (row, column) = (&input[0..7], &input[7..7+3]);
-    let row = binary_search(row.chars().map(Goto::from), 0, 127);
-    let column = binary_search(column.chars().map(Goto::from), 0, 7);
+    let row = binary_search(row.chars(), 0, 127);
+    let column = binary_search(column.chars(), 0, 7);
     (row, column)
 }
 
-fn binary_search(moves: impl Iterator<Item=Goto>, mut low: u32, mut high: u32) -> u32 {
+fn binary_search(moves: Chars, mut low: u32, mut high: u32) -> u32 {
     for goto in moves {
         match goto {
-            Goto::Low => high = low + (high - low) / 2,
-            Goto::High => low = low + (high - low + 1) / 2,
+            'F' | 'L' => high = low + (high - low) / 2,
+            'B' | 'R' => low = low + (high - low + 1) / 2,
+            _ => panic!("Unexpected character {}", goto),
         }
     }
     assert_eq!(high, low);
     low
-}
-
-#[derive(Debug)]
-enum Goto { Low, High }
-
-impl From<char> for Goto {
-    fn from(value: char) -> Goto {
-        match value {
-            'F' | 'L' => Goto::Low,
-            'B' | 'R' => Goto::High,
-            _otherwise => panic!("Unexpected character {}", value),
-        }
-    }
 }
 
 #[cfg(test)]
